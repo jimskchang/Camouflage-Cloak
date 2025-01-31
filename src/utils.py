40b63528578b31e5fcb7ff6f4d2111441ae38fb8
Jim <jimskchang@gmail.com>
2025-01-31 22:23:11 +0800

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import array
+
+def calculate_checksum(data):
+    """
+    Computes checksum for a given data packet (IP or TCP).
+    """
+    if len(data) % 2 != 0:
+        data += b'\0'
+
+    res = sum(array.array("H", data))
+    res = (res >> 16) + (res & 0xffff)
+    res += res >> 16
+
+    return (~res) & 0xffff
