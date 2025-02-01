4aff6cd40b141b67ea51cbb2e9d0e6942278f434
Jim <jimskchang@gmail.com>
2025-02-01 20:15:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import src.settings as settings
 from src.port_deceiver import PortDeceiver
 from src.os_deceiver import OsDeceiver
 
+
 logging.basicConfig(
             format='%(asctime)s [%(levelname)s]: %(message)s',
             datefmt='%y-%m-%d %H:%M',
