7f0af7967bf38c667bccdff5583e48a4a18dd1f7
Jim <jimskchang@gmail.com>
2025-02-05 22:38:58 +0800

    Update Packet.py

--- src/Packet.py
+++ src/Packet.py
@@ -37,12 +37,18 @@ class Packet:
         self.data = data
 
     def unpack(self) -> None:
+        """ Unpacks the received packet into L2, L3, and L4 headers. """
         self.unpack_l2_header()
         self.unpack_l3_header()
+
+        if self.l3 == 'arp':  # ARP has no L4 header, so skip L4 processing
+            return
+
         if self.l4:
             self.unpack_l4_header()
 
     def unpack_l2_header(self) -> None:
+        """ Unpacks Ethernet (L2) header. """
         self.l2_header = self.packet[:14]
         eth = struct.unpack('!6s6sH', self.l2_header)
         eth_dMAC, eth_sMAC, eth_protocol = eth
@@ -61,12 +67,14 @@ class Packet:
         }
 
     def unpack_l3_header(self) -> None:
+        """ Unpacks Layer 3 header based on protocol type. """
         if self.l3 == 'ip':
             self.unpack_ip_header()
         elif self.l3 == 'arp':
-            self.unpack_arp_header()
+            self.unpack_arp_packet()  # Correct ARP processing
 
     def unpack_l4_header(self) -> None:
+        """ Unpacks Layer 4 headers based on protocol type. """
         if self.l4 == 'tcp':
             self.unpack_tcp_header()
         elif self.l4 == 'udp':
@@ -75,6 +83,7 @@ class Packet:
             self.unpack_icmp_header()
 
     def unpack_ip_header(self) -> None:
+        """ Unpacks IP header and determines Layer 4 protocol. """
         self.l3_header = self.packet[14:34]
         fields = struct.unpack('!BBHHHBBH4s4s', self.l3_header)
         IHL_VERSION, _, total_len, _, _, _, PROTOCOL, _, src_IP, dest_IP = fields
@@ -96,7 +105,28 @@ class Packet:
             'dest_IP': convert_bytes_to_ip(dest_IP)
         }
 
+    def unpack_arp_packet(self) -> None:
+        """ Unpacks ARP header and stores relevant fields. """
+        self.l3_header = self.packet[14:42]
+        fields = struct.unpack('!HHBBH6s4s6s4s', self.l3_header)
+        hw_type, proto_type, hw_size, proto_size, opcode, sender_mac, sender_ip, target_mac, target_ip = fields
+
+        self.l3_field = {
+            'hw_type': hw_type,
+            'proto_type': proto_type,
+            'hw_size': hw_size,
+            'proto_size': proto_size,
+            'opcode': opcode,  # 1 = ARP Request, 2 = ARP Reply
+            'sender_mac': convert_bytes_to_mac(sender_mac),
+            'sender_ip': convert_bytes_to_ip(sender_ip),
+            'target_mac': convert_bytes_to_mac(target_mac),
+            'target_ip': convert_bytes_to_ip(target_ip)
+        }
+
+        logging.info(f"ARP Packet: {self.l3_field}")
+
     def unpack_tcp_header(self) -> None:
+        """ Unpacks TCP header. """
         self.l4_header = self.packet[34:54]
         fields = struct.unpack('!HHLLBBHHH', self.l4_header)
         src_port, dest_port, seq, ack_num, offset_flags, flags, _, checksum, _ = fields
@@ -113,6 +143,7 @@ class Packet:
         }
 
     def unpack_udp_header(self) -> None:
+        """ Unpacks UDP header. """
         self.l4_header = self.packet[34:42]
         fields = struct.unpack('!4H', self.l4_header)
         src_port, dest_port, udp_len, checksum = fields
@@ -125,28 +156,24 @@ class Packet:
         }
 
     def pack_l3_header(self):
+        """ Packs the L3 header based on protocol type. """
         if self.l3 == 'ip':
             self.pack_ip_header()
-        elif self.l3 == 'arp':
-            self.pack_arp_header()
         self.packet += self.l3_header
 
     def pack_ip_header(self):
+        """ Packs the IP header for outgoing packets. """
         ip_field = self.l3_field
-
-        # Convert IP addresses to bytes before packing
         src_IP_bytes = convert_ip_to_bytes(ip_field['src_IP'])
         dest_IP_bytes = convert_ip_to_bytes(ip_field['dest_IP'])
 
-        # Pack pseudo IP header
         pseudo_ip_header = struct.pack(
             '!BBHHHBBH4s4s',
             ip_field['IHL_VERSION'], 0, ip_field['total_len'], 0, 0,
             64, ip_field['PROTOCOL'], 0, src_IP_bytes, dest_IP_bytes
         )
-        ip_field['checksum'] = calculate_checksum(pseudo_ip_header)  # Use `calculate_checksum` from utils.py
+        ip_field['checksum'] = calculate_checksum(pseudo_ip_header)
 
-        # Pack final IP header with checksum
         self.l3_header = struct.pack(
             '!BBHHHBBH4s4s',
             ip_field['IHL_VERSION'], 0, ip_field['total_len'], 0, 0,
@@ -154,4 +181,5 @@ class Packet:
         )
 
     def get_proc(self):
+        """ Returns the protocol (L3 or L4) being processed. """
         return self.l3 if not self.l4 else self.l4
