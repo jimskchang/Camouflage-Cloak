7ea95cf18af3a97cad30d53b5ec809637e1374a0
Jim <jimskchang@gmail.com>
2025-02-07 21:14:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('--scan', action="store", help='attacker\'s port scanning technique')
     parser.add_argument('--status', action="store", help='designate port status')
     parser.add_argument('--os', action="store", help='designate os we want to deceive')
-    parser.add_argument('--os_record-dir', action="store", help='Specify output directory for storing OS records')
+    parser.add_argument('--dest', action="store", help='Specify output directory for storing OS records')
     args = parser.parse_args()
     settings.host = args.host
 
@@ -30,24 +30,30 @@ def main():
     if args.scan:
         port_scan_tech = args.scan
 
+        os_record_dir = settings.get_os_record_dir(args.dest)
+
         if port_scan_tech == 'ts':
+            logging.info(f"Executing TS scan on {args.host}, storing results in {os_record_dir}")
             deceiver = OsDeceiver(args.host, args.os)
-            deceiver.os_record(output_path=args.os_record_dir)
+            deceiver.os_record(output_path=os_record_dir)
         elif port_scan_tech == 'od':
             if args.os is None:
                 logging.debug('No os is designated')
             else:
+                logging.info(f"Executing OS deception for {args.host} using {args.os}, storing results in {os_record_dir}")
                 deceiver = OsDeceiver(args.host, args.os)
-                deceiver.os_deceive(output_path=args.os_record_dir)
+                deceiver.os_deceive(output_path=os_record_dir)
         elif port_scan_tech == 'rr':
+            logging.info(f"Recording responses from {args.host}, storing results in {os_record_dir}")
             deceiver = OsDeceiver(args.host, args.os)
-            deceiver.store_rsp(output_path=args.os_record_dir)
+            deceiver.store_rsp(output_path=os_record_dir)
 
         if args.status:
             deceive_status = args.status
             if port_scan_tech == 'pd':
+                logging.info(f"Executing Port Deception for {args.host} with status {deceive_status}, storing results in {os_record_dir}")
                 deceiver = PortDeceiver(args.host)
-                deceiver.deceive_ps_hs(deceive_status, output_path=args.os_record_dir)
+                deceiver.deceive_ps_hs(deceive_status, output_path=os_record_dir)
 
         else:
             logging.debug('No port scan technique is designated')
