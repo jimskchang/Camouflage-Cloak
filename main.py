990e465121717b83bd40483c3a008fa73e3e3322
Jim <jimskchang@gmail.com>
2025-02-07 22:28:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('--scan', action="store", help='attacker\'s port scanning technique')
     parser.add_argument('--status', action="store", help='designate port status')
     parser.add_argument('--os', action="store", help='designate os we want to deceive')
-    parser.add_argument('-dest', action="store", FILE_ADDRESS specify file storage location')    
+    parser.add_argument('--dest', action="store", FILE_ADDRESS specify file storage location')    
     args = parser.parse_args()
     settings.host = args.host
 
