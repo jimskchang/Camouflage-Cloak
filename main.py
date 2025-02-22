b3f941f39d0c09f555e7ac6aa32a2195a183d416
Jim <jimskchang@gmail.com>
2025-02-22 14:53:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def collect_fingerprint(target_host, dest, max_packets=100):
     logging.info(f"Storing data in: {os_dest}")
 
     try:
+        packet, addr = sock.recvfrom(65565)
     packet, addr = sock.recvfrom(65565)
     except socket.timeout:
     logging.warning("No packets received within timeout period. Exiting scan.")
@@ -153,3 +154,5 @@ def main():
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%y-%m-%d %H:%M", level=logging.DEBUG)
     main()
+
+ 
