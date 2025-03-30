169bdc7818cc56ae40ab87e119651f00ee2ed01e
Jim <jimskchang@gmail.com>
2025-03-30 23:12:57 +0800

    Update response.py

--- src/response.py
+++ src/response.py
@@ -6,52 +6,49 @@ def synthesize_response(pkt, template: bytes, ttl: int = 64, window: int = 8192,
     try:
         scapy_pkt = Ether(template)
 
-        # --- IP Layer Handling ---
         if IP in scapy_pkt:
-            scapy_pkt[IP].dst = pkt.l3_field.get("src_IP_str", pkt.l3_field.get("src_IP", "0.0.0.0"))
-            scapy_pkt[IP].src = pkt.l3_field.get("dest_IP_str", pkt.l3_field.get("dest_IP", "0.0.0.0"))
+            scapy_pkt[IP].dst = pkt.l3_field.get("src_IP_str", pkt.l3_field.get("src_IP"))
+            scapy_pkt[IP].src = pkt.l3_field.get("dest_IP_str", pkt.l3_field.get("dest_IP"))
             scapy_pkt[IP].ttl = ttl
             scapy_pkt[IP].id = deceiver.get_ip_id(pkt.l3_field.get("src_IP_str", "")) if deceiver else random.randint(0, 65535)
-            scapy_pkt[IP].tos = deceiver.os_flags.get("tos", 0) if deceiver else 0
+
+            tos = deceiver.os_flags.get("tos", 0) if deceiver else 0
+            ecn = deceiver.os_flags.get("ecn", 0) if deceiver else 0
+            scapy_pkt[IP].tos = tos | ecn
+
             if deceiver and deceiver.os_flags.get("df", False):
                 scapy_pkt[IP].flags = "DF"
-            else:
-                scapy_pkt[IP].flags = pkt.l3_field.get("FRAGMENT_STATUS", 0)
-            del scapy_pkt[IP].chksum  # Force checksum recalc
 
-        # --- TCP Layer Handling ---
+            del scapy_pkt[IP].chksum
+
         if TCP in scapy_pkt:
             scapy_pkt[TCP].sport = pkt.l4_field.get("dest_port", 1234)
             scapy_pkt[TCP].dport = pkt.l4_field.get("src_port", 1234)
-            scapy_pkt[TCP].seq = pkt.l4_field.get("ack_num", random.randint(0, 2**32 - 1))
+            scapy_pkt[TCP].seq = pkt.l4_field.get("ack_num", random.randint(0, 2**32-1))
             scapy_pkt[TCP].ack = pkt.l4_field.get("seq", 0) + 1
-            scapy_pkt[TCP].flags = pkt.l4_field.get("flags", "SA")
+            scapy_pkt[TCP].flags = "SA"
             scapy_pkt[TCP].window = window
 
             if deceiver:
                 ts_echo = pkt.l4_field.get("option_field", {}).get("ts_val", 0)
-                src_ip_str = pkt.l3_field.get("src_IP_str", "0.0.0.0")
-                scapy_pkt[TCP].options = deceiver.get_tcp_options(src_ip_str, ts_echo=ts_echo)
+                src_ip = pkt.l3_field.get("src_IP_str", "")
+                scapy_pkt[TCP].options = deceiver.get_tcp_options(src_ip, ts_echo=ts_echo)
 
-            # TCP Reserved bits are typically 3 bits padded in the offset/flags field; scapy manages it internally
             del scapy_pkt[TCP].chksum
 
-        # --- UDP Layer Handling ---
         if UDP in scapy_pkt:
             scapy_pkt[UDP].sport = pkt.l4_field.get("dest_port", 1234)
             scapy_pkt[UDP].dport = pkt.l4_field.get("src_port", 1234)
             del scapy_pkt[UDP].chksum
 
-        # --- ICMP Layer Handling ---
         if ICMP in scapy_pkt:
-            scapy_pkt[ICMP].type = 0  # Echo Reply
+            scapy_pkt[ICMP].type = 0
             del scapy_pkt[ICMP].chksum
 
-        # --- ARP Layer Handling ---
         if ARP in scapy_pkt:
-            scapy_pkt[ARP].op = 2  # ARP reply
-            scapy_pkt[ARP].psrc = pkt.l3_field.get("recv_ip_str", pkt.l3_field.get("dest_IP_str", "0.0.0.0"))
-            scapy_pkt[ARP].pdst = pkt.l3_field.get("send_ip_str", pkt.l3_field.get("src_IP_str", "0.0.0.0"))
+            scapy_pkt[ARP].op = 2
+            scapy_pkt[ARP].psrc = pkt.l3_field.get("recv_ip_str", pkt.l3_field.get("dest_IP"))
+            scapy_pkt[ARP].pdst = pkt.l3_field.get("send_ip_str", pkt.l3_field.get("src_IP"))
 
         return bytes(scapy_pkt)
 
