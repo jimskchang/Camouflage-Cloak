465c05366a5dccfe73664c7a7a53f4e0625ace4c
Jim <jimskchang@gmail.com>
2025-04-04 11:57:26 +0800

    Update os_deceiver.py

--- src/os_deceiver.py
+++ src/os_deceiver.py
@@ -17,11 +17,64 @@ from src.Packet import Packet
 from src.tcp import TcpConnect
 from src.response import synthesize_response
 from src.fingerprint_utils import gen_key
+from collections import defaultdict
 
 DEBUG_MODE = os.environ.get("DEBUG", "0") == "1"
 UNMATCHED_LOG = os.path.join(settings.OS_RECORD_PATH, "unmatched_keys.log")
 
 class OsDeceiver:
+    def templateSynthesis(packet, proto_type, template_dict, pair_dict, host_ip):
+    """
+    Build OS fingerprint templates from observed packets using normalized request/response pairs.
+
+    Parameters:
+    - packet: unpacked Packet object
+    - proto_type: 'TCP', 'UDP', 'ICMP', 'ARP'
+    - template_dict: dict storing templates for each proto
+    - pair_dict: dict mapping request pairs to normalized keys
+    - host_ip: local host IP as string
+
+    Returns:
+    - template_dict: updated with new normalized request-response pairs
+    """
+    try:
+        src_ip = packet.l3_field.get("src_IP_str")
+        dst_ip = packet.l3_field.get("dest_IP_str")
+        src_port = packet.l4_field.get("src_port")
+        dst_port = packet.l4_field.get("dest_port")
+
+        if proto_type == "TCP":
+            pair = (src_ip, dst_ip, src_port, dst_port)
+        elif proto_type == "ICMP":
+            pair = packet.l4_field.get("icmp_id", 0)
+        elif proto_type == "ARP":
+            pair = (src_ip, dst_ip)
+        elif proto_type == "UDP":
+            pair = (src_ip, dst_ip, src_port, dst_port)
+        else:
+            return template_dict
+
+        if dst_ip == host_ip:
+            key = generateKey(packet, proto_type)
+            pair_dict[pair] = key
+            if key not in template_dict[proto_type]:
+                template_dict[proto_type][key] = None
+
+        elif src_ip == host_ip and pair in pair_dict:
+            if proto_type == "ICMP" and packet.l4_field.get("icmp_type") == 3:
+                key = generateKey(packet, "UDP")
+                if key not in template_dict["UDP"]:
+                    template_dict["UDP"][key] = None
+            else:
+                key = pair_dict[pair]
+                template_dict[proto_type][key] = packet.packet
+
+        return template_dict
+
+    except Exception as e:
+        logging.warning(f"⚠️ templateSynthesis error: {e}")
+        return template_dict
+    
     def __init__(self, target_host: str, target_os: str, dest=None, nic: str = None):
         self.host = target_host
         self.os = target_os
